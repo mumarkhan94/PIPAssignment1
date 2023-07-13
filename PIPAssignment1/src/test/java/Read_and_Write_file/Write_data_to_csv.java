6175280fe2866ebe65a3f396b37b7c4c4a4235c2
Umar Khan, Mohd <mumarkhan@deloitte.com>
2023-07-13 12:21:22 +0530

    Added proper comment in Write_data_to_csc file

--- PIPAssignment1/src/test/java/Read_and_Write_file/Write_data_to_csv.java
+++ PIPAssignment1/src/test/java/Read_and_Write_file/Write_data_to_csv.java
@@ -16,34 +16,32 @@ public class Write_data_to_csv {
 	public static void Enterdata(Object[][] empdata) throws IOException {
 
 		File myFile = new File(".\\DataFile\\Employe1.xlsx");
-		FileInputStream inputStream = new FileInputStream(myFile);
+		FileInputStream inputStream = new FileInputStream(myFile);  // Select the file.
 		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 		System.out.println(" wb: " + workbook);
-		XSSFSheet sheet = workbook.getSheet("Sheet1");
+		XSSFSheet sheet = workbook.getSheet("Sheet1");  // Select the sheet name.
 
-		// its contain all type of data
-//		Object empdata[][] = { { "salamna", "09654", "QA", "m" }, { "sofiya", "342524", "SDET2", "f" } };
-		int rows = empdata.length;
-		int cols = empdata[0].length;
+		int rows = empdata.length;  // Get the number of rows present in sheet.
+		int cols = empdata[0].length; // Get the number of column present in first row.
 		int sheetrows = sheet.getLastRowNum(); // Get the number of row already present
 
-		for (int r = 0; r < rows; r++) {
+		for (int r = 0; r < rows; r++) {  // This for loop is used for row iteration.
 			XSSFRow row = sheet.createRow(r + sheetrows + 1);
-			for (int c = 0; c < cols; c++) {
-				XSSFCell cell = row.createCell(c);
+			for (int c = 0; c < cols; c++) {  // This for loop is used for column iteration.
+				XSSFCell cell = row.createCell(c);  // Create the new cell.
 				Object value = empdata[r][c];
 
-				if (value instanceof String)
+				if (value instanceof String)  //  If the value is String then this if condition will execute.
 					cell.setCellValue((String) value);
-				if (value instanceof Integer)
+				if (value instanceof Integer) // If the value is integer then this if condition will execute.
 					cell.setCellValue((Integer) value);
 			}
 
 		}
 
 		FileOutputStream os = new FileOutputStream(myFile);
-		workbook.write(os);
-		inputStream.close();
+		workbook.write(os);  // Write the data in sheet.
+		inputStream.close(); // close the sheet.
 		System.out.println("Data inserted");
 	
 	
@@ -53,19 +51,20 @@ public class Write_data_to_csv {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//Take the input.
 		Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int rows = scanner.nextInt();
+        int rows = scanner.nextInt();  // Take number of row.
         System.out.print("Enter the number of columns: ");
-        int columns = scanner.nextInt();
+        int columns = scanner.nextInt(); // Take number of cloumn.
         Object[][] arr = new Object[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 System.out.print("Enter the value for row " + (i + 1) + ", column " + (j + 1) + ": ");
-                arr[i][j] = scanner.next();
+                arr[i][j] = scanner.next(); // Enter the data and storing the data in object of array.
             }
         }
-        
+        // Print the data enter by user.
         System.out.println("The data entered is:");
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
@@ -73,7 +72,7 @@ public class Write_data_to_csv {
             }
             System.out.println();
         }
-        Enterdata(arr);
+        Enterdata(arr); // Calling function and passing object of array.
         
     }
 
