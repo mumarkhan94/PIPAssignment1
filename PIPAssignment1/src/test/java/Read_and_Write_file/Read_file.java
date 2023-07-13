f0c547e14858090b6eee9e249a4b1e574791931c
Umar Khan, Mohd <mumarkhan@deloitte.com>
2023-07-13 12:10:01 +0530

    Added proper comment in Read_file

--- PIPAssignment1/src/test/java/Read_and_Write_file/Read_file.java
+++ PIPAssignment1/src/test/java/Read_and_Write_file/Read_file.java
@@ -16,31 +16,31 @@ public class Read_file {
 		String excelFilePath = ".\\DataFile\\Employe1.xlsx";
 		FileInputStream inputstream = new FileInputStream(excelFilePath);
 
-		XSSFWorkbook workbook = new XSSFWorkbook(inputstream); // access the file
+		XSSFWorkbook workbook = new XSSFWorkbook(inputstream); // Access the file
 
-		XSSFSheet sheet = workbook.getSheet("Sheet1"); // select the first sheet with the help of sheet name
+		XSSFSheet sheet = workbook.getSheet("Sheet1"); // Select the first sheet with the help of sheet name
 
-		int rows = sheet.getLastRowNum(); // get the number of row
-		int cols = sheet.getRow(1).getLastCellNum(); // get the number of column
+		int rows = sheet.getLastRowNum(); // Get the number of row
+		int cols = sheet.getRow(1).getLastCellNum(); // Get the number of column.
 
-		for (int r = 0; r <= rows; r++) // First for loop representing the row
+		for (int r = 0; r <= rows; r++) // First for loop representing the row.
 		{
-			XSSFRow row = sheet.getRow(r);
-			for (int c = 0; c < cols; c++) // Second for loop representing the column
+			XSSFRow row = sheet.getRow(r); // Accessing row one by one.
+			for (int c = 0; c < cols; c++) // Second for loop representing the column.
 			{
-				XSSFCell cell = row.getCell(c);
-				XSSFCell gender = row.getCell(3);
-				String g = gender.getStringCellValue();
-				if (g.toLowerCase().equals("f") || r == 0) {
+				XSSFCell cell = row.getCell(c); // Accessing the column one by one.
+				XSSFCell gender = row.getCell(3); // This line is used for accessing the gender column.
+				String g = gender.getStringCellValue(); // Assign the gender value in local variable so we can compare.
+				if (g.toLowerCase().equals("f") || r == 0) {  // r == 0 is used for to print the first line from csv.
 					switch (cell.getCellType()) {
-					case STRING: {
+					case STRING: { // This switch case will execute when the cell data is string.
 						System.out.print(cell.getStringCellValue() + " | ");
-						if (cell.getStringCellValue().toLowerCase().equals("f")
+						if (cell.getStringCellValue().toLowerCase().equals("f")  // Comparing gender.
 								|| cell.getStringCellValue().toLowerCase().equals("gender"))
 							System.out.println();
 						break;
 					}
-					case NUMERIC:
+					case NUMERIC: // This switch case will execute when the cell data is integer.
 						System.out.print(cell.getNumericCellValue() + " | ");
 						break;
 					}
