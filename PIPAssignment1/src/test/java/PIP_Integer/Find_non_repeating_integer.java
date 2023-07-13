6bd529f37ccd12e2d97c7410caff5b185b4cf935
Umar Khan, Mohd <mumarkhan@deloitte.com>
2023-07-13 11:49:43 +0530

    Added proper comment in Find_non_repeating_integer file

--- PIPAssignment1/src/test/java/PIP_Integer/Find_non_repeating_integer.java
+++ PIPAssignment1/src/test/java/PIP_Integer/Find_non_repeating_integer.java
@@ -2,14 +2,14 @@ package PIP_Integer;
 
 public class Find_non_repeating_integer {
 	
-	 public static void findNonRepeatingIntegers(int[] arr) {
-	        for (int i = 0; i < arr.length; i++) { 
+	 public static void findNonRepeatingIntegers(int[] arr) {  //Creating public static method so its can access from any class without object.
+	        for (int i = 0; i < arr.length; i++) {
 	            int current = arr[i];
 	            boolean isRepeating = false;
 
 	         // Check if the current element appears elsewhere in the array
 	            for (int j = 0; j < arr.length; j++) {
-	                if (i != j && current == arr[j]) {
+	                if (i != j && current == arr[j]) { // i != j is use because i don't compare same index.
 	                    isRepeating = true;
 	                    break;
 	                }
@@ -26,7 +26,7 @@ public class Find_non_repeating_integer {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		int[] testArray = {1,2,3,4,5,4,5,2,1};
-     findNonRepeatingIntegers(testArray); // Calling non integer repeating method.
+     findNonRepeatingIntegers(testArray); // Calling non-integer repeating method.
 		
 }
 
