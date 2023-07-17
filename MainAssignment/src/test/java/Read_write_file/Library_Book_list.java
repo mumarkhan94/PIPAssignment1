49342b403a5a24144e0d8d7565d638a41bc9bc44
Umar Khan, Mohd <mumarkhan@deloitte.com>
2023-07-17 21:09:31 +0530

    Added Book search method in Library_book_list file.

---
+++ MainAssignment/src/test/java/Read_write_file/Library_Book_list.java
@@ -0,0 +1,92 @@
+package Read_write_file;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.*;
+
+public class Library_Book_list {
+
+    private static final int STRING = 1;
+    private static final int NUMERIC = 0;
+
+    public void Book_list() throws IOException {
+        // TODO Auto-generated method stub
+        String excelFilePath = ".\\DataFile\\Book_details.xlsx";
+        FileInputStream inputstream = new FileInputStream(excelFilePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(inputstream); // Access the file
+
+        XSSFSheet sheet = workbook.getSheet("Sheet1"); // Select the first sheet with the help of sheet name
+
+        int rows = sheet.getLastRowNum(); // Get the number of row
+        int cols = sheet.getRow(1).getLastCellNum(); // Get the number of column.
+        Object[][] arr = new Object[1][5];
+        for (int r = 0; r <= rows; r++) // First for loop representing the row.
+        {
+            XSSFRow row = sheet.getRow(r); // Accessing row one by one.
+            for (int c = 0; c < cols; c++) // Second for loop representing the column.
+            {
+                XSSFCell cell = row.getCell(c); // Accessing the column one by one.
+                switch (cell.getCellType()) {
+                    case STRING: { // This switch case will execute when the cell data is string.
+                        System.out.print(cell.getStringCellValue() + " | ");
+                        break;
+                    }
+                    case NUMERIC: // This switch case will execute when the cell data is integer.
+                        System.out.print(cell.getNumericCellValue() + " | ");
+                        break;
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public void Search_Book(int cell_number, String Search_text) throws IOException {
+        // TODO Auto-generated method stub
+        String excelFilePath = ".\\DataFile\\Book_details.xlsx";
+        FileInputStream inputstream = new FileInputStream(excelFilePath);
+
+        XSSFWorkbook workbook = new XSSFWorkbook(inputstream); // Access the file
+
+        XSSFSheet sheet = workbook.getSheet("Sheet1"); // Select the first sheet with the help of sheet name
+
+        int rows = sheet.getLastRowNum(); // Get the number of row
+        int cols = sheet.getRow(1).getLastCellNum(); // Get the number of column.
+
+        for (int r = 0; r <= rows; r++) // First for loop representing the row.
+        {
+            XSSFRow row = sheet.getRow(r); // Accessing row one by one.
+            for (int c = 0; c < cols; c++) // Second for loop representing the column.
+            {
+                XSSFCell cell = row.getCell(c); // Accessing the column one by one.
+                XSSFCell gender = row.getCell(cell_number); // This line is used for accessing the gender column.
+                String g = gender.getStringCellValue(); // Assign the gender value in local variable so we can compare.
+                if (g.toLowerCase().equals(Search_text.toLowerCase()) || r == 0) {  // r == 0 is used for to print the first line from csv.
+                    switch (cell.getCellType()) {
+                        case STRING: { // This switch case will execute when the cell data is string.
+                            System.out.print(cell.getStringCellValue() + " | ");
+                            if (cell.getStringCellValue().toLowerCase().equals("f")  // Comparing gender.
+                                    || cell.getStringCellValue().toLowerCase().equals("gender"))
+                                System.out.println();
+                            break;
+                        }
+                        case NUMERIC: // This switch case will execute when the cell data is integer.
+                            System.out.print(cell.getNumericCellValue() + " | ");
+                            break;
+                    }
+                }
+            }
+        System.out.println();
+        }
+    }
+
+}
+
+
+
+
--- MainAssignment/src/test/java/Utility_package_for_comman_use/LibraryManagementSystem.java
+++ MainAssignment/src/test/java/Utility_package_for_comman_use/LibraryManagementSystem.java
@@ -1,6 +1,7 @@
 package Utility_package_for_comman_use;
 
 
+import Read_write_file.Library_Book_list;
 import Read_write_file.Register_users;
 
 
@@ -32,7 +33,8 @@ public class LibraryManagementSystem {
         System.out.println("2. Borrow Book");
         System.out.println("3. Return Book");
         System.out.println("4. Search Books");
-        System.out.println("5. Exit");
+        System.out.println("5. Book List");
+        System.out.println("6. Exit");
         System.out.print("Enter your choice: ");
         int choice = scanner.nextInt();
 
@@ -49,6 +51,51 @@ public class LibraryManagementSystem {
                 break;
             case 4:
                 // TODO: Implement search books functionality
+                Search_Book_By();
+                break;
+            case 5:
+            {
+                Library_Book_list library_book_list=new Library_Book_list();
+                library_book_list.Book_list();
+                break;
+            }
+            case 6:
+                System.out.println("Exiting the program. Goodbye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                displayMainMenu();
+        }
+    }
+
+
+    public static void Search_Book_By() throws IOException {
+        Library_Book_list library_book_list=new Library_Book_list();
+        System.out.println("\nSearch_Book_By:");
+        System.out.println("1. Book title:");
+        System.out.println("2. Author:");
+        System.out.println("3. ISBN:");
+        System.out.println("4. Genre:");
+        System.out.println("5. Exit:");
+        System.out.print("Enter your choice: ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter Book title:");
+                library_book_list.Search_Book(0,scanner.next());
+                break;
+            case 2:
+                System.out.println("Enter Author Name:");
+                library_book_list.Search_Book(1,scanner.next());
+                break;
+            case 3:
+                System.out.println("Enter ISBN:");
+                library_book_list.Search_Book(2,scanner.next());
+                break;
+            case 4:
+                System.out.println("Enter Genre:");
+                library_book_list.Search_Book(3,scanner.next());
                 break;
             case 5:
                 System.out.println("Exiting the program. Goodbye!");
@@ -58,6 +105,12 @@ public class LibraryManagementSystem {
                 System.out.println("Invalid choice. Please try again.");
                 displayMainMenu();
         }
+
+
+
+
     }
+
+
 }
 
